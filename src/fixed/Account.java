97a5a01ee70ef1169b6ce054c9b451455a176c34
kabl1001 <kabl1001@stud.hs-kl.de>
2024-08-12 18:35:28 +0200

    Klasse Account für die Berechnung mit Geldbeträgen hinzugefügt.

--- src/fixed/Account.java
+++ src/fixed/Account.java
@@ -9,11 +9,6 @@ public class Account extends FixedPointNumber{
     super(s, v);
   }
 
-  public void multiply (double x){
-    FixedPointNumber faktor2 = new FixedPointNumber(this.scaling, this.displacement, x);
-    this.value *= (int) ((double) (faktor2.getValue() + this.displacement) /this.scaling);
-  }
-
   public void write() {
     System.out.println("Test: " + this.getValue());
   }
