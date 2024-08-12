b4921b4b2f38b0c5f11e005b91b28de16b71ab84
kabl1001 <kabl1001@stud.hs-kl.de>
2024-08-12 18:52:44 +0200

    Anwendungsklasse AppAccount hinzugefügt, in der Beispiele für die Verwendung der Klasse Account realisiert werden.

---
+++ src/fixed/AppAccount.java
@@ -0,0 +1,36 @@
+package fixed;
+
+public class AppAccount {
+
+  private static Account lengths2;
+
+  public static void main(String[] args) {
+
+    System.out.println("Messwerte für Multiply");
+
+    int s = 100;
+    int v = 0;
+    lengths2 = new Account(s, v);
+
+    double x = 2.0;
+    lengths2.set(x);
+    print();
+    System.out.println("Die ganze Zahl dazu: " + lengths2.getValue());
+
+    double y = 4.0;
+    lengths2.multiply(y);
+
+    System.out.println("Das Ergebnis der Multiplikation von 2.0 und 4.0");
+    print();
+    System.out.println("Die ganze Zahl dazu: " + lengths2.getValue());
+
+    lengths2.write();
+  }
+
+  private static FixedPointNumber lengths;
+
+  private static void print() {
+    System.out.format("%.3f%n", lengths2.get());
+  }
+
+}
